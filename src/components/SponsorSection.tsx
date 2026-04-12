98189c1fc26ee4cb7f8038f008b8f9db8b46dedf
shramanb113 <shramanb1@gmail.com>
2026-04-12 18:44:40 +0530

    changed dkms

--- src/components/SponsorSection.tsx
+++ src/components/SponsorSection.tsx
@@ -80,7 +80,7 @@ export default function SponsorsSection() {
                 <Image src="/dkms.webp" alt="DKMS" fill className="object-contain object-center drop-shadow-[0_0_15px_rgba(255,215,0,0.2)] scale-105" />
               </div>
 
-              <p className="text-gray-400 text-sm sm:text-base font-medium tracking-wide max-w-sm mb-8 leading-relaxed">DK Machine Service delivers top-tier machining and maintenance with precision, reliability, and craftsmanship you can truly count on.</p>
+              <p className="text-gray-400 text-sm sm:text-base font-medium tracking-wide max-w-sm mb-8 leading-relaxed">DKMS is a global non-profit organization dedicated to fighting blood cancer and blood disorders, such as thalassemia and aplastic anemia, by registering potential blood stem cell donors.</p>
             </div>
 
             <div className="z-10 relative mt-auto">
